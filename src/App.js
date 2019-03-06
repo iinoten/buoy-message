6525e04d5d353a251180592a5d83123cde399577
Iino Ten <nteten0313@icloud.com>
2019-03-06 11:36:01 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
 
+import Point from './components/Points/Points';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDqvgfjo0TFGcec0aIDfqo-2MoQcdimKYs",
+  authDomain: "buoy-message.firebaseapp.com",
+  databaseURL: "https://buoy-message.firebaseio.com",
+  projectId: "buoy-message",
+  storageBucket: "buoy-message.appspot.com",
+  messagingSenderId: "325897667671"
+});
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Point />
+        Helloworld
       </div>
     );
   }

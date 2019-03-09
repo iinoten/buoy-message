86db5f52b0b857c99e8274b015ab556bbead68eb
Iino Ten <nteten0313@icloud.com>
2019-03-10 06:50:17 +0900

    Update funtion of post message able to be change db

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import firebase from 'firebase';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
 
-import Point from './components/Points/Points';
-import helloRouter from './components/Hello_router/helloRouter';
-import Buoy from './components/Buoy/Buoy';
+import Post from './components/Post/Post';
 
-firebase.initializeApp({
-  apiKey: "AIzaSyDqvgfjo0TFGcec0aIDfqo-2MoQcdimKYs",
-  authDomain: "buoy-message.firebaseapp.com",
-  databaseURL: "https://buoy-message.firebaseio.com",
-  projectId: "buoy-message",
-  storageBucket: "buoy-message.appspot.com",
-  messagingSenderId: "325897667671"
-});
-var db = firebase.firestore();
+
 class App extends Component {
   render() {
     return (
-      <Buoy />
+      <Post />
     );
   }
 }

738edc9cab91eeb06a087075642f9ea1dc59b5d7
Iino Ten <nteten0313@icloud.com>
2019-03-13 23:47:09 +0900

    Add TopPage

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
 
+import TopPage from './components/TopPage/TopPage';
 import Post from './components/Post/Post';
 
 
 class App extends Component {
   constructor() {
     super();
-    console.log(navigator.userAgent);
   }
   render() {
     return (
-      <Post />
+      <TopPage />
     );
   }
 }

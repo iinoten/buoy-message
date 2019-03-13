65a00d726d9b7e146a683b653981164d6a0fb328
Iino Ten <nteten0313@icloud.com>
2019-03-13 22:58:22 +0900

    Add topPage File

---
+++ src/components/TopPage/TopPage.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
+
+import './TopPage.css'
+
+class TopPage extends Component{
+  render(){
+    return(
+      <div></div>
+    );
+  }
+}
+
+export default TopPage;

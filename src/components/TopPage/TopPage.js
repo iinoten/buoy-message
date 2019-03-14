dc7475e2dc2f38a08b417feba1907ebdb3e18085
Iino Ten <nteten0313@icloud.com>
2019-03-14 14:48:50 +0900

    Update Drifting buoy separation other file

--- src/components/TopPage/TopPage.js
+++ src/components/TopPage/TopPage.js
@@ -1,19 +1,8 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
+import Drifting from '../Drifting/Drifting';
 
 import './TopPage.css'
-
-class Drifting extends Component{
-  render(){
-    return(
-      <div id="Drifting">
-        <div id="Drifting-rod" />
-        <div id="Drifting-frag" />
-        <div id="baloon" />
-      </div>
-    );
-  }
-}
 
 class TopPage extends Component{
   render(){

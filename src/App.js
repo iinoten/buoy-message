be9e2d0f394d67864e693fc57208452656cf0d25
Iino Ten <nteten0313@icloud.com>
2019-03-15 21:03:39 +0900

    Add get buoy from data of latitude

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import {BrowserRouter, Route, Link, Switch} from 'react-router-dom';
+import firebase from './Firebase.js';
 
 import TopPage from './components/TopPage/TopPage';
 import Post from './components/Post/Post';
-
 
 class App extends Component {
   constructor() {
--- src/components/Level/Level.js
+++ src/components/Level/Level.js
@@ -2,8 +2,43 @@ import React, {Component} from 'react';
 
 import Drifting from '../Drifting/Drifting';
 import './Level.css'
+import firebase from 'firebase'
+
+import update from 'immutability-helper';
+var db = firebase.firestore();
 
 class Level extends Component{
+  constructor() {
+    super();
+    this.state = {
+      buoys: null,  //浮かせるブイ達
+      position: {
+        x: null,
+        y: null
+      }
+    }
+    navigator.geolocation.watchPosition(this.get_position);
+  }
+  get_position = (position) => {
+    console.log(position);
+    this.setState({
+      position: {
+        x: Math.round(position.coords.latitude * 100000),
+        y: Math.round(position.coords.longitude * 100000)
+      }
+    })
+    console.log(this.state.position);
+    db.collection("buoys").where("position.latitude", "<", this.state.position.x + 200).where("position.latitude", ">", this.state.position.y - 200)
+      .get()
+      .then((query_snapshot) => {
+        query_snapshot.forEach((doc) => {
+          console.log(doc.id,"and...",doc.data().message);
+        })
+      })
+  }
+  componentDidMount(){
+    navigator.geolocation.watchPosition(this.get_position);
+  }
   render(){
     return(
       <div id="Level">

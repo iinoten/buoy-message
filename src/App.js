9b2a5866aed9b43dc75076bdc1f880035145053f
Iino Ten <nteten0313@icloud.com>
2019-03-20 00:30:39 +0900

    Add mobile view

--- src/App.js
+++ src/App.js
@@ -15,18 +15,11 @@ class App extends Component {
     }
   }
   componentDidMount() {
-    var getDevice = (function(){
-      var ua = navigator.userAgent;
-      if(ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0){
-          return 'sp';
-      }else if(ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0){
-          return 'tab';
-      }else{
-          return 'other';
-      }
-    })();
-    if(getDevice === 'other') {
+    if(navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/i)){
+      // スマホ・タブレット（iOS・Android）の場合の処理を記述
+    }else{
       this.setState({ is_mobile: false })
+      // PCの場合の処理を記述
     }
   }
   render() {

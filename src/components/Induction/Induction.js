6c0ca9b8f0a95f879bc15864d16690aa2e1b0c3d
Iino Ten <nteten0313@icloud.com>
2019-03-20 04:23:50 +0900

    Add Induct PC to mobile component

--- src/components/Induction/Induction.js
+++ src/components/Induction/Induction.js
@@ -5,7 +5,14 @@ import './Induction.css'
 class Induction extends Component{
   render(){
     return(
-      <div>スマホ向けのコンテンツだよ!!</div>
+      <div>
+        <div id="induction-title">buoy</div>
+        <div id="induction-subtitle">- インターネットのうみにメッセージをうかべよう -</div>
+        <div id="induction-buoy">
+          <img src={`${process.env.PUBLIC_URL}/qrl_picture.png`} alt="QRcode" id="induction_QR_code"/>
+        </div>
+        <div id="description-induction">QRコードをスマホでよみこんで<span className="highlight">buoy</span>をたいけんしよう</div>
+      </div>
     );
   }
 }
